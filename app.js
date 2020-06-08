38961991af92323d83c05045918957ff8ac04eb9
Sumit Yadav <careeratedjing@gmail.com>
2020-06-08 16:27:09 +0530

    allowed CORS

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
